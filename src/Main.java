a99f73aacea0dbb80216b3baed283029eefbeba1
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-09-21 09:29:41 -0400

    Dice Rolling Sim

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //variables
+        int numSides = 0;
+        int roll = 0;
+
+        //ask the user for number of sides on the die
+        System.out.println("How many sides are on your chosen die?");
+        if (scan.hasNextInt()) {
+            numSides = scan.nextInt();
+        }
+        else {
+            System.out.println("You have entered an invalid number of sides.");
+            System.exit(0);
+        }
+
+        //calculate a random number from 1-# of sides on the die
+        roll = (int)(Math.random()*numSides)+1;
+
+        //output the roll
+        System.out.println("You rolled: " + roll);
+
     }
 }
